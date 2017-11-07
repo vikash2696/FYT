a9a90710b3fdfa00ca03562c90b1c6a449507afa
vikash2696 <vikash2696@gmail.com>
2017-11-07 09:14:55 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+// server.js
+// load the things we need
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page 
+app.get('/', function(req, res) {
+	var title = 'Learning node';
+	res.render('pages/index', {
+		title : title
+	});
+});
+
+// app.get('/', function(req, res) {
+//     res.render('pages/index');
+// });
+
+// about page 
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+app.listen(8080);
+console.log('8080 is the magic port');
